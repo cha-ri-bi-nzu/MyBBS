e30da92ef0ca5a14fc92b4800b1b1a532c634871
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2024-06-20 16:48:35 +0900

    [add]トップページ仮作成
    確認の為タイトルと見出しのみ表示

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>MyBBS</title>
+  </head>
+  <body>
+    <header>
+      <h1>MyBBS</h1>
+    </header>
+  </body>
+</html>
