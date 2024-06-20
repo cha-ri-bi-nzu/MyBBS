b80175d3c456bae5386e5a2cd910255a91dd432e
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2024-06-20 18:03:12 +0900

    [fix]タイプミスの修正
    不要な「&」が入っていた為削除

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ $username = "ppfpkadai_kouya";
 $password = "193th62kc3kijou";
 $dbname = "ppfpkadai_test";
 
-$conn = new mysqli($servername, $username, $password, &$dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
   die("接続失敗：" . $conn->connect_error);
